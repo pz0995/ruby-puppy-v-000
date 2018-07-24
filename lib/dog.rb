38d2683cc740c9db796256b7746e39f1efc54f15
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-24 01:38:48 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  attr_accessor :name, :clear_all
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all <<  self
+  end
+
+  def self.all
+
+    puts @@all.map { |dog| dog.name }
+
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
